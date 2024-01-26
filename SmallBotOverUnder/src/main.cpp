3ee87d5747cd71f75b03be46db18ad8ebe83e6cd
ref020 <149557406+ref020@users.noreply.github.com>
2024-01-26 14:32:51 -0600

    Consolidated Daniel's code, and the auton, into the same file.

--- SmallBotOverUnder/src/main.cpp
+++ SmallBotOverUnder/src/main.cpp
@@ -1,113 +1,123 @@
 #include "main.h"
+#include <iostream>
+using namespace std;
 
+// Controller Setup
 pros::Controller master(pros::E_CONTROLLER_MASTER);
-pros::Motor frontLeft(19);
-pros::Motor frontRight(12, true);
-pros::Motor cata(18);
-pros::Motor backLeft(20);
-pros::Motor backRight(11, true);
-pros::Motor_Group left({frontLeft, backLeft});
-pros::Motor_Group right({frontRight, backRight});
-pros::Motor leftLift(5, true);
-pros::Motor rightLift(6);
-pros::Motor_Group lift({leftLift, rightLift});
+
+
+// Motor Initialization
+pros::Motor catapult(18);
+
+pros::Motor frontLeftMotor(19, MOTOR_GEAR_BLUE, true, MOTOR_ENCODER_COUNTS);
+pros::Motor frontRightMotor(12, MOTOR_GEAR_BLUE, false, MOTOR_ENCODER_COUNTS);
+pros::Motor backLeftMotor(20, MOTOR_GEAR_BLUE, true, MOTOR_ENCODER_COUNTS);
+pros::Motor backRightMotor(11, MOTOR_GEAR_BLUE, false, MOTOR_ENCODER_COUNTS);
+
+pros::Motor_Group leftSideMotors({frontLeftMotor, backLeftMotor});
+pros::Motor_Group rightSideMotors({frontRightMotor, backRightMotor});
+
+pros::Motor leftLiftMotor(5, true);
+pros::Motor rightLiftMotor(6);
+pros::Motor_Group lift({leftLiftMotor, rightLiftMotor});
+
 pros::Motor intake(1, true);
-pros::ADIDigitalIn limitSwitch (8);
+
+// Sensor Initialization
+pros::ADIAnalogIn lineTracker('H');
+pros::ADIUltrasonic ultrasonic('E', 'F');
+
+// Globals
 static bool skills = false;
 static bool red = false;
 static bool blue = false;
 
-/*
-Subroutines:
-Below are the subroutines used in the opcontrol section
-The following subroutines are used:
--move distance (feet)
--launch triball
--prime catapult
-*/
 
-void movement() {
+// User Drive Function
+void drive() {
 	int power = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
 	int turn = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
 	
-	int leftP = power + turn;
-	int rightP = power - turn;
-
-	int mx = std::max(std::abs(leftP), std::abs(rightP));
-
-	if(mx > 127){
-		leftP = (leftP / mx) * 127;
-		rightP = (rightP / mx) * 127;
-	}
-
-	left = leftP;
-	right = rightP;
+	leftSideMotors = power + turn;
+	rightSideMotors = power - turn;
 }
 
+// Move x distance
+void moveDistance(int distance, int speed) {
+    double targetDistanceLeft = frontLeftMotor.get_position() + distance;
+    double targetDistanceRight = frontRightMotor.get_position() + distance;
+
+    // leftDrive.move_absolute(targetDistanceLeft, speed);
+    // rightDrive.move_absolute(targetDistanceRight, speed);
+
+    leftSideMotors.move_relative(distance, speed);
+    rightSideMotors.move_relative(distance, speed);
+
+    while (abs(abs(targetDistanceLeft) - abs(frontLeftMotor.get_position())) > 20) {
+    }
+
+	
+    leftSideMotors.move_relative(0, speed);
+    rightSideMotors.move_relative(0, speed);
+    pros::delay(200);
+}
+
+// Target angle is relative to the robot's current heading, so a 90 will mean the robot will turn 90 degrees
+void rotateToHeading(int angle, int speed) {
+    // the distance the motor needs to travel in order to reach the desired rotation
+    double distance = 2335 * ((angle - 5) / 360.0);
+
+    // the actual target encoder value for the leftDrive
+    double targetDistanceLeft = frontLeftMotor.get_position() + distance;
+    double targetDistanceRight = frontRightMotor.get_position() - distance;
+
+    // set the motors to move to the target positions. (This will move and stop the motors on its own [apparently])
+    leftSideMotors.move_absolute(targetDistanceLeft, speed);
+    rightSideMotors.move_absolute(targetDistanceRight, speed);
+
+    while (abs(abs(targetDistanceLeft) - abs(frontLeftMotor.get_position())) > 50) {
+    }
+    
+    pros::delay(200);
+}
+
+// Checks to see if a triball is loaded
+bool triballLoaded() {
+    return ultrasonic.get_value() < 50;
+}
+
+// Makes sure the catapult is in the proper position to fire
 void primeCatapult() {
-
-	while (limitSwitch.get_value() == 0){
-		cata.move(50);
-
-		//copy of drive controls, allowing for motion and operation of other subsystems
-
-		movement(); 
-
-		//Lift Controls
-		//UP = Raise Lift Arm
-		//DOWN = Lower Lift Arm/Raise Robot
-		if (master.get_digital(DIGITAL_UP)) {
-
-			lift.move_velocity(100);
-
-			movement();
-
-		} else if (master.get_digital(DIGITAL_DOWN)) {
-
-			lift.move_velocity(-100);
-
-			movement();
-
-		} else {
-
-			lift.set_brake_modes(pros::E_MOTOR_BRAKE_HOLD);
-			lift.brake();
-			
-		}
-
-
-		//Intake code
-		//L1 = Intake
-		//L2 = Output
-		if (master.get_digital(DIGITAL_L1)) {
-
-			intake.move_velocity(100);
-			movement();
-		
-		} else if (master.get_digital(DIGITAL_L2)){
-
-			intake.move_velocity(-100);
-			movement();
-
-		} else{
-
-			intake.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-			intake.brake();
-
+	int lineTrackerVal;
+	lineTrackerVal = lineTracker.get_value();
+	if (lineTrackerVal > 300) {
+		catapult.move(100);
+		while(lineTrackerVal > 300) {
+			drive();
+			lineTrackerVal = lineTracker.get_value();
+			if (lineTrackerVal < 300) {
+				catapult.move(0);
+				catapult.set_brake_mode(MOTOR_BRAKE_HOLD);
+			}
 		}
 	}
-
-	
+	else {
+		catapult.move(0);
+		catapult.set_brake_mode(MOTOR_BRAKE_HOLD);
+	}
 }
 
+// Fires the triball
 void launchTriball() {
-
-		cata.move(100);
-		pros::delay(20);
-		primeCatapult();
-	
+	catapult.move(100);
+	pros::delay(200);
+	primeCatapult();
 }
 
+void launchTriballNoPrime() {
+	catapult.move(100);
+	pros::delay(200);
+	}
 
 /**
  * A callback function for LLEMU's center button.
@@ -167,6 +177,14 @@ void initialize() {
 	pros::lcd::register_btn1_cb(on_center_button);
 	pros::lcd::register_btn0_cb(on_right_button);
 	pros::lcd::register_btn2_cb(on_left_button);
+
+	frontLeftMotor.set_brake_mode(MOTOR_BRAKE_BRAKE);
+	frontRightMotor.set_brake_mode(MOTOR_BRAKE_BRAKE);
+	backLeftMotor.set_brake_mode(MOTOR_BRAKE_BRAKE);
+	backRightMotor.set_brake_mode(MOTOR_BRAKE_BRAKE);
+
+	leftLiftMotor.set_encoder_units(MOTOR_ENCODER_ROTATIONS);
+	lift.set_brake_modes(MOTOR_BRAKE_HOLD);
 }
 
 /**
@@ -205,7 +223,66 @@ void competition_initialize() {
  * from where it left off.
  */
 void autonomous() {
-	
+	lift.move(127);
+	pros::delay(300);
+	lift.move(0);
+	rotateToHeading(-55, 100);
+	launchTriballNoPrime();
+	primeCatapult();
+	rotateToHeading(230, 200);
+	intake.move(40);
+	lift.move(-127);
+	pros::delay(50);
+	lift.move(0);
+	moveDistance(-4200, 300);
+	lift.move(-127);
+	pros::delay(50);
+	lift.move(0);
+	rotateToHeading(-45, 100);
+	moveDistance(-1400, 300);
+	rotateToHeading(-45, 100);
+	// lift.move(127);
+	// pros::delay(300);
+	// lift.move(0);
+	intake.move(-127);
+	moveDistance(400, 300);
+	lift.move(-127);
+	pros::delay(1000);
+	lift.move(0);
+	moveDistance(-500, 600);
+	moveDistance(500, 300);
+	intake.move(0);
+	lift.move(127);
+	pros::delay(500);
+	lift.move(0);
+	rotateToHeading(-135, 200);
+	moveDistance(-500, 200);
+	rotateToHeading(-90, 200);
+	moveDistance(-300, 200);
+	intake.move(70);
+	lift.move(-127);
+	moveDistance(-300, 200);
+	pros::delay(200);
+	lift.move(0);
+	moveDistance(600, 200);
+	intake.move(0);
+	rotateToHeading(-90, 200);
+	moveDistance(-600, 200);
+	rotateToHeading(-40, 200);
+	lift.move(127);
+	pros::delay(500);
+	lift.move(0);
+	moveDistance(-200, 200);
+	moveDistance(500, 200);
+	lift.move(-127);
+	pros::delay(500);
+	lift.move(0);
+	moveDistance(-700, 400);
+	moveDistance(500, 200);
+	rotateToHeading(-135, 250);
+	moveDistance(-1200, 250);
+	rotateToHeading(55, 250);
+	moveDistance(-4000, 250);
 
 }
 
@@ -224,73 +301,63 @@ void autonomous() {
  */
 
 void opcontrol() {
-	
+	float liftPos;
 	primeCatapult();
+	leftLiftMotor.tare_position();
 
 	while (true) {
+		if (master.get_digital(DIGITAL_A)) {
+			autonomous();
+		}
+		
+		pros::lcd::set_text(0, "Drivetrain Left Encoder: " + to_string(frontLeftMotor.get_position()));
+
+		liftPos = leftLiftMotor.get_position();
+		pros::lcd::print(2, "%f", liftPos);
 
 		//Split Arcade control code
-		movement();
-        
-		//Catapult code
-		//R1 = Fire Cata
-		
-        if (master.get_digital(DIGITAL_R1)) {
+		drive();
 
-        	launchTriball();
-			movement(); 
-			
-		} else {
-		
-		    cata.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-			cata.brake();
-
+		// This block launches the triball when the correct buttons is pressed
+		if (master.get_digital(DIGITAL_R2) && triballLoaded()) {
+			launchTriball();
 		}
-		
-		//Lift Controls
-		//UP = Raise Lift Arm
-		//DOWN = Lower Lift Arm/Raise Robot
+
+		// This block lifts the lift and intake up and down
 		if (master.get_digital(DIGITAL_UP)) {
-
-			lift.move_velocity(100);
-
-			movement();
-
-		} else if (master.get_digital(DIGITAL_DOWN)) {
-
-			lift.move_velocity(-100);
-
-			movement();
-
-		} else {
-
-			lift.set_brake_modes(pros::E_MOTOR_BRAKE_HOLD);
-			lift.brake();
-			
+			if (liftPos < 2 || master.get_digital(DIGITAL_B)) {
+				lift.move(127);
+			}
+			else {
+				lift.move(0);
+			}
+		}
+		else if (master.get_digital(DIGITAL_DOWN)) {
+			if (liftPos > 0) {
+				lift.move(-127);
+			}
+			else {
+				lift.move(0);
+			}
+		}
+		else {
+			lift.move(0);
 		}
 
-
-		//Intake code
-		//L1 = Intake
-		//L2 = Output
+		// This block operates the intake motors
 		if (master.get_digital(DIGITAL_L1)) {
-
-			intake.move_velocity(100);
-			movement();
-		
-		} else if (master.get_digital(DIGITAL_L2)){
-
-			intake.move_velocity(-100);
-			movement();
-
-		} else{
-
-			intake.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-			intake.brake();
-
+			intake.move(127);
 		}
+		else if (master.get_digital(DIGITAL_L2)) {
+			intake.move(-127);
+		}
+		else {
+			intake.move(0);
+		}
+
+
+
 		pros::delay(20);
 	}
 }	
-
 
